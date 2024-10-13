4681f8e2a38c2b4e8308fae316dd7a67ebb09046
kkerry49 <kingkerryizerestudentmorocco@gmail.com>
2024-10-13 12:43:50 +0100

    Update main.py111

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,8 +1,13 @@
-def main():
-    # Ecrivez votre code ici !
-    # Attention tout votre code doit être indenté comme ce commentaire
+nombre_a_gauche = 5
+nombre_a_droite = 10
+symbole = "addition"
+resultat = 0
 
-
-# Ne touchez pas le code ci-dessous
-if __name__ == "__main__":
-    main()
+match symbole :
+      case "addition":
+          resultat=nombre_a_gauche + nombre_a_droite
+        print(f"{nombre_a_gauche} + {nombre_a_droite} ={resultat}")
+      case "soustraction":
+      case "multiplication":
+      case "division":
+          
